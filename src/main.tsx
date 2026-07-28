4b7dbe6dea43062f7332ef55de85a9711ed62642
Ezequiel <ingepasillasg@gmail.com>
2026-07-27 18:08:17 -0600

    feat:  instalacion de react router
    feat: inicio de ruteo

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { HeroesApp } from "./HeroesApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Holi</h1>
+    <HeroesApp />
   </StrictMode>,
 );
---
+++ src/router/app.router.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router";
+import { HomePage } from "../heroes/pages/home/HomePage.tsx";
+import { HeroePage } from "../heroes/pages/heroe/HeroePage.tsx";
+import { SearchPage } from "../heroes/pages/search/SearchPage.tsx";
+import { AdminPage } from "../admin/pages/AdminPage.tsx";
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+
+  {
+    path: "/heroe/1",
+    element: <HeroePage />,
+  },
+
+  {
+    path: "/search",
+    element: <SearchPage />,
+  },
+  {
+    path: "/admin",
+    element: <AdminPage />,
+  },
+]);
