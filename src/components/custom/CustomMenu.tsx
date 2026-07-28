b5929782007c7cf971e1ec11ce3b2b9f421b83fe
Ezequiel <ingepasillasg@gmail.com>
2026-07-28 16:14:30 -0600

    feat: custom menu

---
+++ src/components/custom/CustomMenu.tsx
@@ -0,0 +1,43 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { Link, useLocation } from "react-router";
+
+export const CustomMenu = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => pathname === path;
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {/* Home */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={cn(isActive("/") && "bg-slate-200 ", "p-2 rounded-md")}
+            render={<Link to="/" />}
+          >
+            Home
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        {/* Search */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={cn(
+              isActive("/search") && "bg-slate-200 ",
+              "p-2 rounded-md",
+            )}
+            render={<Link to="/search" />}
+          >
+            Search SuperHeroes
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
